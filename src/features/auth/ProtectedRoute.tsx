1ac9c7a7701e750616fbcc4ed460fd098edf9436
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2022-11-19 19:13:00 +0100

    feat: add docker

--- src/features/auth/ProtectedRoute.tsx
+++ src/features/auth/ProtectedRoute.tsx
@@ -1,10 +1,11 @@
 import {useSelector} from "react-redux";
 import {RootState} from "../../store";
 import {Navigate} from "react-router-dom";
+import {AuthStatus} from "./authSlice";
 
 export default function ProtectedRoute(props: { children: any }) {
-    if (useSelector((state: RootState) => state.auth.isAuth))
+    if (useSelector((state: RootState) => state.auth.status) === AuthStatus.LOGGED_IN) {
         return props.children;
-    else
-        return <Navigate to={"/login"}/>;
+    }
+    return <Navigate to="/login"/>
 }
