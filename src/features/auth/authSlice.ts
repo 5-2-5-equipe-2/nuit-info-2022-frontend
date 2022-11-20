43969d61a98c075b352389cd117095a5025d7109
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2022-11-20 12:34:40 +0100

    feat: simplify auth slice and add netlify deployment settings

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -41,54 +41,30 @@ export const authSlice = createSlice({
             state.accessToken = action.payload.accessToken;
             state.refreshToken = action.payload.refreshToken;
             state.id = action.payload.id;
-            state.expires = new Date(action.payload.expires).toISOString();
+            state.expires = action.payload.expires.toISOString();
             state.status = AuthStatus.LOGGED_IN;
-            state.error = null;
         })
         builder.addCase(loginAction.rejected, (state, action) => {
             state.status = AuthStatus.ERROR;
             state.error = action.error.message || "Unknown error";
-            state.accessToken = "";
-            state.refreshToken = "";
-            state.id = -1;
-            state.expires = "";
 
         })
         builder.addCase(loginAction.pending, (state) => {
             state.status = AuthStatus.LOGGING_IN;
-            state.error = null;
-            state.accessToken = "";
-            state.refreshToken = "";
-            state.id = -1;
-            state.expires = "";
 
         })
 
         builder.addCase(logoutAction.fulfilled, (state) => {
-                state.accessToken = "";
-                state.refreshToken = "";
-                state.id = -1;
-                state.expires = "";
                 state.status = AuthStatus.LOGGED_OUT;
-                state.error = null;
             }
         )
         builder.addCase(logoutAction.rejected, (state, action) => {
             state.status = AuthStatus.ERROR;
             state.error = action.error.message || "Unknown error";
-            state.accessToken = "";
-            state.refreshToken = "";
-            state.id = -1;
-            state.expires = "";
 
         })
         builder.addCase(logoutAction.pending, (state) => {
             state.status = AuthStatus.LOGGING_OUT;
-            state.error = null;
-            state.accessToken = "";
-            state.refreshToken = "";
-            state.id = -1;
-            state.expires = "";
 
         })
 
@@ -98,7 +74,6 @@ export const authSlice = createSlice({
             state.id = action.payload.id;
             state.expires = action.payload.expires.toISOString();
             state.status = AuthStatus.LOGGED_IN;
-            state.error = null;
         });
         builder.addCase(refreshTokenAction.rejected, (state, action) => {
             state.status = AuthStatus.ERROR;
@@ -107,7 +82,6 @@ export const authSlice = createSlice({
 
         builder.addCase(refreshTokenAction.pending, (state) => {
             state.status = AuthStatus.REFRESHING_TOKEN;
-            state.error = null;
 
         });
 
